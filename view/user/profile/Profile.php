ed51b6abc76c24f15ea856a54bc95527225a9e28
SammyGambler <samanAzizi1394@gmail.com>
2023-03-28 20:48:55 +0330

    refactor: hack file changed to php

--- view/user/profile/Profile.php
+++ view/user/profile/Profile.php
@@ -1,11 +1,11 @@
+<?php $profile_title = 'پروفایل کاربری'; ?>
 <div class="container wrapper-free-styles wrapper-margin-top">
 	<div class="row">
 		<div class="col-sm-12 col-md-12 col-lg-12" id="profile-content">
-			<h3>پروفایل کاربری</h3>
+			<h3><?php echo $profile_title; ?></h3>
 			<div class="col-sm-2 col-md-2 col-lg-2" id="profile-menu">
 				<ul class="nav nav-tabs tabs-left" id="profile-nav">
-					<li class="active"><a href="#home-profile" data-toggle="tab"><i
-									class="material-icons">perm_identity</i>شناسنامه شما</a></li>
+					<li class="active"><a href="#home-profile" data-toggle="tab"><i class="material-icons">perm_identity</i>شناسنامه شما</a></li>
 					<li><a href="#profile-course-list" data-toggle="tab"><i class="material-icons">import_contacts</i>فاکتور
 							های شما</a>
 					</li>
@@ -14,8 +14,7 @@
 					<li><a href="#profile-user-all-tickets" data-toggle="tab"><i class="material-icons">record_voice_over</i>پیام
 							های
 							شما</a></li>
-					<li><a href="#profile-user-transaction" data-toggle="tab"><i
-									class="material-icons">monetization_on</i>تراکنش
+					<li><a href="#profile-user-transaction" data-toggle="tab"><i class="material-icons">monetization_on</i>تراکنش
 							های شما</a></li>
 				</ul>
 			</div>
@@ -27,39 +26,35 @@
 						</div>
 						<form class="form-horizontal" id="profile-form">
 							<div class="form-group input-container">
-								
+
 								<div class="col-sm-10 col-sm-offset-1 input_container">
-									<input type="email" class="form-control input-lg"
-									       placeholder="نام خود را وارد نمایید...">
+									<input type="email" class="form-control input-lg" placeholder="نام خود را وارد نمایید...">
 									<i class="material-icons input-icon">perm_identity</i>
 								</div>
 							</div>
-							
+
 							<div class="form-group input-container">
-								
+
 								<div class="col-sm-10 col-sm-offset-1 input_container">
-									<input type="email" class="form-control input-lg"
-									       placeholder="نام خانوادگی خود را وارد نمایید...">
+									<input type="email" class="form-control input-lg" placeholder="نام خانوادگی خود را وارد نمایید...">
 									<i class="material-icons input-icon">supervisor_account</i>
 								</div>
 							</div>
 							<div class="form-group input-container">
-								
+
 								<div class="col-sm-10 col-sm-offset-1 input_container">
-									<input type="email" class="form-control input-lg"
-									       placeholder="نام کاربری خود را وارد نمایید...">
+									<input type="email" class="form-control input-lg" placeholder="نام کاربری خود را وارد نمایید...">
 									<i class="material-icons input-icon">face</i>
 								</div>
 							</div>
 							<div class="form-group input-container">
-								
+
 								<div class="col-sm-10 col-sm-offset-1 input_container">
-									<input type="email" class="form-control input-lg"
-									       placeholder="ایمیل خود را وارد نمایید...">
+									<input type="email" class="form-control input-lg" placeholder="ایمیل خود را وارد نمایید...">
 									<i class="material-icons input-icon">email</i>
 								</div>
 							</div>
-							
+
 							<div class="form-group input-container">
 								<div class=" col-sm-12 text-center" style="margin-top: 20px;margin-bottom: 5px;">
 									<div class="col-sm-6 col-sm-offset-3">
@@ -71,87 +66,87 @@
 					</div>
 					<div class="tab-pane" id="profile-course-list">
 						<ul class="list-group">
-							
+
 							<li><a href="#"><i class="material-icons">import_contacts</i>
 									<table class="factor-tbl">
 										<thead>
-										<tr>
-											<th>ردیف</th>
-											<th>شماره فاکتور</th>
-											<th>تاریخ ثبت فاکتور</th>
-											<th>قیمت فاکتور</th>
-											<th>نوع سکه</th>
-										</tr>
+											<tr>
+												<th>ردیف</th>
+												<th>شماره فاکتور</th>
+												<th>تاریخ ثبت فاکتور</th>
+												<th>قیمت فاکتور</th>
+												<th>نوع سکه</th>
+											</tr>
 										</thead>
 										<tbody>
-										
-										<tr>
-											<td>1</td>
-											<td>1234</td>
-											<td>98/11/1</td>
-											<td>98/11/1</td>
-											<td>بهار آزادی</td>
-										</tr>
+
+											<tr>
+												<td>1</td>
+												<td>1234</td>
+												<td>98/11/1</td>
+												<td>98/11/1</td>
+												<td>بهار آزادی</td>
+											</tr>
 										</tbody>
-									
+
 									</table>
-								
-								
+
+
 								</a></li>
 							<li><a href="#"><i class="material-icons">import_contacts</i>
 									<table class="factor-tbl">
 										<thead>
-										<tr>
-											<th>ردیف</th>
-											<th>شماره فاکتور</th>
-											<th>تاریخ ثبت فاکتور</th>
-											<th>قیمت فاکتور</th>
-											<th>نوع سکه</th>
-										</tr>
+											<tr>
+												<th>ردیف</th>
+												<th>شماره فاکتور</th>
+												<th>تاریخ ثبت فاکتور</th>
+												<th>قیمت فاکتور</th>
+												<th>نوع سکه</th>
+											</tr>
 										</thead>
 										<tbody>
-										
-										<tr>
-											<td>1</td>
-											<td>1234</td>
-											<td>98/11/1</td>
-											<td>98/11/1</td>
-											<td>بهار آزادی</td>
-										</tr>
+
+											<tr>
+												<td>1</td>
+												<td>1234</td>
+												<td>98/11/1</td>
+												<td>98/11/1</td>
+												<td>بهار آزادی</td>
+											</tr>
 										</tbody>
-									
+
 									</table>
-								
-								
+
+
 								</a></li>
 							<li><a href="#"><i class="material-icons">import_contacts</i>
 									<table class="factor-tbl">
 										<thead>
-										<tr>
-											<th>ردیف</th>
-											<th>شماره فاکتور</th>
-											<th>تاریخ ثبت فاکتور</th>
-											<th>قیمت فاکتور</th>
-											<th>نوع سکه</th>
-										</tr>
+											<tr>
+												<th>ردیف</th>
+												<th>شماره فاکتور</th>
+												<th>تاریخ ثبت فاکتور</th>
+												<th>قیمت فاکتور</th>
+												<th>نوع سکه</th>
+											</tr>
 										</thead>
 										<tbody>
-										
-										<tr>
-											<td>1</td>
-											<td>1234</td>
-											<td>98/11/1</td>
-											<td>98/11/1</td>
-											<td>بهار آزادی</td>
-										</tr>
+
+											<tr>
+												<td>1</td>
+												<td>1234</td>
+												<td>98/11/1</td>
+												<td>98/11/1</td>
+												<td>بهار آزادی</td>
+											</tr>
 										</tbody>
-									
+
 									</table>
-								
-								
+
+
 								</a></li>
-						
-						
+
+
 						</ul>
 					</div>
 					<div class="tab-pane" id="profile-user-tickets">
@@ -193,10 +188,8 @@
 								</div>
 								<div id="section1" class="panel-collapse collapse in custom-panel-collapse">
 									<div class="ticket-details-wrapper">
-										<img class="img-circle sender-ticket-image" src=""><span>ارسال شده در تاریخ : 20-12-1396</span><span><a
-													href="#ticket-answer1" class="btn btn-warning send-answer-ticket"
-													data-toggle="collapse">ارسال پاسخ</a></span>
-										
+										<img class="img-circle sender-ticket-image" src=""><span>ارسال شده در تاریخ : 20-12-1396</span><span><a href="#ticket-answer1" class="btn btn-warning send-answer-ticket" data-toggle="collapse">ارسال پاسخ</a></span>
+
 										<div class="ticket-text">
 											<p>
 												نظر من اینه
@@ -208,8 +201,7 @@
 													پس از بررسی پاسخ داده خواهد شد...
 												</div>
 												<div class="form-group">
-                                                    <textarea class="form-control"
-                                                              rows="10">dadasdasdasdasdsad</textarea>
+													<textarea class="form-control" rows="10">dadasdasdasdasdsad</textarea>
 												</div>
 												<div class="form-group text-left">
 													<input type="submit" class="btn btn-success" value="ارسال پیام">
@@ -218,10 +210,8 @@
 										</div>
 									</div>
 									<div class="ticket-details-wrapper">
-										<img class="img-circle sender-ticket-image" src=""><span>ارسال شده در تاریخ : 20-12-1396</span><span><a
-													href="#ticket-answer2" class="btn btn-warning send-answer-ticket"
-													data-toggle="collapse">ارسال پاسخ</a></span>
-										
+										<img class="img-circle sender-ticket-image" src=""><span>ارسال شده در تاریخ : 20-12-1396</span><span><a href="#ticket-answer2" class="btn btn-warning send-answer-ticket" data-toggle="collapse">ارسال پاسخ</a></span>
+
 										<div class="ticket-text">
 											<p>
 												نظر من اینه
@@ -233,8 +223,7 @@
 													پس از بررسی پاسخ داده خواهد شد...
 												</div>
 												<div class="form-group">
-                                                    <textarea class="form-control"
-                                                              rows="10">dadasdasdasdasdsad</textarea>
+													<textarea class="form-control" rows="10">dadasdasdasdasdsad</textarea>
 												</div>
 												<div class="form-group text-left">
 													<input type="submit" class="btn btn-success" value="ارسال پیام">
@@ -242,10 +231,10 @@
 											</form>
 										</div>
 									</div>
-								
+
 								</div>
 							</div>
-						
+
 						</div>
 					</div>
 					<div class="tab-pane " id="profile-user-transaction">
