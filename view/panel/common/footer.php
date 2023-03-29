6ab397c7ab88ac4bb692c2c06f9fb18ee49f976d
SammyGambler <samanAzizi1394@gmail.com>
2023-03-29 19:42:25 +0330

    footer fixed

--- view/panel/common/footer.php
+++ view/panel/common/footer.php
@@ -1,12 +1,16 @@
+<?php
+$copyright = "کلیه حقوق سایت متعلق به <a href='www.puzzle-sekke.ir'>سازنده</a> می باشد.";
+?>
 <div class="clearfix"></div>
 <div class="container-fluid" id="footer">
     <div class="row">
         <div class="col-sm-12">
 
-            <p>کلیه حقوق سایت متعلق به <a href="www.puzzle-sekke.ir">سازنده</a> می باشد.</p>
+            <p><?php echo $copyright; ?></p>
 
         </div>
     </div>
 </div>
 </body>
+
 </html>
